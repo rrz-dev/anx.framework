2f3ab2541f6085a50884317f9435ccf2fca5eb2f
SND\rene87_cp <SND\rene87_cp@7a4bfd9b-e295-4cbd-a51f-ab472bcf69b7>
2011-11-04 23:06:39 +0000

    Keyboard and Mouse Classe

---
+++ ANX.Framework/Input/Keyboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ANX.Framework.Input
+{
+    public static class Keyboard
+    {
+        public static KeyboardState GetState() 
+        { 
+            throw new NotImplementedException(); 
+        }
+        public static KeyboardState GetState (PlayerIndex playerIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ANX.Framework/Input/Mouse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ANX.Framework.Input
+{
+    public static class Mouse
+    {
+        public static IntPtr WindowHandle { get; set; }
+        public static MouseState GetState() 
+        {
+            throw new NotImplementedException();
+        }
+        public static void SetPosition(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
