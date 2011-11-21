0d6f5d549dc61d8fda85ea85491bdf36833b5609
Glatzemann <glatzemann@users.noreply.github.com>
2011-11-21 16:33:22 +0000

    replaced XNA icon with an own one (dummy)

--- Samples/WindowsGame/AddInSelector.Designer.cs
+++ Samples/WindowsGame/AddInSelector.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddInSelector));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -142,6 +143,7 @@
             this.Controls.Add(this.button1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "AddInSelector";
             this.Text = "ANX.Framework AddIn selection...";
