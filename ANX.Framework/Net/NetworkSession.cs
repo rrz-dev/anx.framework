e5d82e7b98df356fe799aa59bd4965f428298836
SND\AstrorEnales_cp <SND\AstrorEnales_cp@7a4bfd9b-e295-4cbd-a51f-ab472bcf69b7>
2012-01-18 09:00:35 +0000

    Fixed last classes signature (now we are identical to XNA)

--- ANX.Framework/Net/NetworkSession.cs
+++ ANX.Framework/Net/NetworkSession.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using ANX.Framework.GamerServices;
+using System.Collections.Generic;
 
 #region License
 
@@ -9,7 +11,7 @@
 //
 //
 //
-// Microsoft Public License (Ms-PL)
+// Microsoft public License (Ms-PL)
 //
 // This license governs use of the accompanying software. If you use the software, you accept this license. 
 // If you do not accept the license, do not use the software.
@@ -54,7 +56,186 @@ namespace ANX.Framework.Net
 		public const int MaxSupportedGamers = 31;
 		public const int MaxPreviousGamers = 100;
 
+		public event EventHandler<NetworkSessionEndedEventArgs> SessionEnded;
+		public event EventHandler<GamerJoinedEventArgs> GamerJoined;
+		public event EventHandler<GamerLeftEventArgs> GamerLeft;
+		public event EventHandler<GameStartedEventArgs> GameStarted;
+		public event EventHandler<GameEndedEventArgs> GameEnded;
+		public event EventHandler<HostChangedEventArgs> HostChanged;
+		public event EventHandler<InviteAcceptedEventArgs> InviteAccepted;
+		public event EventHandler<WriteLeaderboardsEventArgs> WriteArbitratedLeaderboard;
+		public event EventHandler<WriteLeaderboardsEventArgs> WriteTrueSkill;
+		public event EventHandler<WriteLeaderboardsEventArgs> WriteUnarbitratedLeaderboard;
+
 		public bool IsDisposed
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool AllowJoinInProgress
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool AllowHostMigration
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool IsEveryoneReady
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool IsHost
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public NetworkSessionType SessionType
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public NetworkSessionState SessionState
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int BytesPerSecondSent
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int BytesPerSecondReceived
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public NetworkSessionProperties SessionProperties
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public TimeSpan SimulatedLatency
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public float SimulatedPacketLoss
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int PrivateGamerSlots
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int MaxGamers
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public NetworkGamer Host
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public GamerCollection<NetworkGamer> AllGamers
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public GamerCollection<LocalNetworkGamer> LocalGamers
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public GamerCollection<NetworkGamer> RemoteGamers
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public GamerCollection<NetworkGamer> PreviousGamers
 		{
 			get
 			{
@@ -88,6 +269,137 @@ namespace ANX.Framework.Net
 		}
 
 		public void ResetReady()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void AddLocalGamer(SignedInGamer gamer) 
+		{
+			throw new NotImplementedException();
+		}
+
+		public NetworkGamer FindGamerById(byte gamerId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static NetworkSession EndJoinInvited(IAsyncResult result)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static NetworkSession Create(NetworkSessionType sessionType,
+			int maxLocalGamers, int maxGamers)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static NetworkSession Create(NetworkSessionType sessionType,
+			int maxLocalGamers, int maxGamers, int privateGamerSlots,
+			NetworkSessionProperties sessionProperties)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static NetworkSession Create(NetworkSessionType sessionType,
+			IEnumerable<SignedInGamer> localGamers, int maxGamers, int privateGamerSlots,
+			NetworkSessionProperties sessionProperties)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static IAsyncResult BeginCreate(NetworkSessionType sessionType,
+			int maxLocalGamers, int maxGamers, AsyncCallback callback, object asyncState)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static IAsyncResult BeginCreate(NetworkSessionType sessionType,
+			int maxLocalGamers, int maxGamers, int privateGamerSlots,
+			NetworkSessionProperties sessionProperties, AsyncCallback callback,
+			object asyncState)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static IAsyncResult BeginCreate(NetworkSessionType sessionType,
+			IEnumerable<SignedInGamer> localGamers, int maxGamers, int privateGamerSlots,
+			NetworkSessionProperties sessionProperties, AsyncCallback callback,
+			object asyncState)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static NetworkSession EndCreate(IAsyncResult result)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static AvailableNetworkSessionCollection Find(NetworkSessionType sessionType,
+			int maxLocalGamers, NetworkSessionProperties searchProperties)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static AvailableNetworkSessionCollection Find(NetworkSessionType sessionType,
+			IEnumerable<SignedInGamer> localGamers, NetworkSessionProperties searchProperties)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static IAsyncResult BeginFind(NetworkSessionType sessionType,
+			int maxLocalGamers, NetworkSessionProperties searchProperties,
+			AsyncCallback callback, object asyncState)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static IAsyncResult BeginFind(NetworkSessionType sessionType,
+			IEnumerable<SignedInGamer> localGamers, NetworkSessionProperties searchProperties,
+			AsyncCallback callback, object asyncState)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static AvailableNetworkSessionCollection EndFind(IAsyncResult result)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static NetworkSession Join(AvailableNetworkSession availableSession)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static IAsyncResult BeginJoin(AvailableNetworkSession availableSession,
+			AsyncCallback callback, object asyncState)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static NetworkSession EndJoin(IAsyncResult result)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static NetworkSession JoinInvited(int maxLocalGamers)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static NetworkSession JoinInvited(IEnumerable<SignedInGamer> localGamers)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static IAsyncResult BeginJoinInvited(int maxLocalGamers,
+			AsyncCallback callback, object asyncState)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static IAsyncResult BeginJoinInvited(IEnumerable<SignedInGamer> localGamers,
+			AsyncCallback callback, object asyncState)
 		{
 			throw new NotImplementedException();
 		}
