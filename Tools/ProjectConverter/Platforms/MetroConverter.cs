bf97e6ba69fd19abf454c865331fac25d0567546
SND\AstrorEnales_cp <SND\AstrorEnales_cp@7a4bfd9b-e295-4cbd-a51f-ab472bcf69b7>
2012-08-12 21:43:28 +0000

    Updated Metro Converter and fixed a missing project reference

--- Tools/ProjectConverter/Platforms/MetroConverter.cs
+++ Tools/ProjectConverter/Platforms/MetroConverter.cs
@@ -5,6 +5,8 @@ namespace ProjectConverter.Platforms
 {
 	public class MetroConverter : Converter
 	{
+		private bool isCurrentProjectExecutable;
+
 		public override string Postfix
 		{
 			get
@@ -39,20 +41,43 @@ namespace ProjectConverter.Platforms
 			ChangeOrCreateNodeValue(element, "ProjectTypeGuids",
 				"{BC8A1FFA-BEE3-4634-8014-F334798102B3};" +
 				"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}");
-			ChangeOrCreateNodeValue(element, "TargetFrameworkVersion", "4.5");
-			ChangeOrCreateNodeValue(element, "TargetPlatformVersion", "8.0");
+
+			//ChangeOrCreateNodeValue(element, "TargetFrameworkVersion", "4.5");
+			//ChangeOrCreateNodeValue(element, "TargetPlatformVersion", "8.0");
+			DeleteNodeIfExists(element, "TargetFrameworkVersion");
+			DeleteNodeIfExists(element, "TargetPlatformVersion");
+
 			DeleteNodeIfExists(element, "TargetFrameworkProfile");
 			DeleteNodeIfExists(element, "XnaFrameworkVersion");
 			DeleteNodeIfExists(element, "XnaPlatform");
 			DeleteNodeIfExists(element, "XnaProfile");
 			DeleteNodeIfExists(element, "XnaCrossPlatformGroupID");
 			DeleteNodeIfExists(element, "XnaOutputType");
+			DeleteNodeIfExists(element, "ApplicationIcon");
+			DeleteNodeIfExists(element, "Thumbnail");
+			DeleteNodeIfExists(element, "PublishUrl");
+			DeleteNodeIfExists(element, "Install");
+			DeleteNodeIfExists(element, "UpdateEnabled");
+			DeleteNodeIfExists(element, "UpdateMode");
+			DeleteNodeIfExists(element, "UpdateInterval");
+			DeleteNodeIfExists(element, "UpdateIntervalUnits");
+			DeleteNodeIfExists(element, "UpdatePeriodically");
+			DeleteNodeIfExists(element, "UpdateRequired");
+			DeleteNodeIfExists(element, "MapFileExtensions");
+			DeleteNodeIfExists(element, "ApplicationRevision");
+			DeleteNodeIfExists(element, "ApplicationVersion");
+			DeleteNodeIfExists(element, "IsWebBootstrapper");
+			DeleteNodeIfExists(element, "UseApplicationTrust");
+			DeleteNodeIfExists(element, "BootstrapperEnabled");
 
 			XElement outputTypeNode = GetOrCreateNode(element, "OutputType");
 			string outputTypeValue = outputTypeNode.Value.ToLower();
-			if (outputTypeValue == "winexe" ||
+
+			isCurrentProjectExecutable =
+				outputTypeValue == "winexe" ||
 				outputTypeValue == "appcontainerexe" ||
-				outputTypeValue == "exe")
+				outputTypeValue == "exe";
+			if (isCurrentProjectExecutable)
 			{
 				outputTypeNode.Value = "AppContainerExe";
 			}
@@ -73,10 +98,27 @@ namespace ProjectConverter.Platforms
 		}
 		#endregion
 
+		#region ConvertPropertyGroup
+		protected override void ConvertPropertyGroup(XElement element)
+		{
+			DeleteNodeIfExists(element, "NoStdLib");
+			DeleteNodeIfExists(element, "XnaCompressContent");
+		}
+		#endregion
+
 		#region Convert
 		protected override void PostConvert()
 		{
 			string namespaceName = currentProject.Root.Name.NamespaceName;
+			
+			AddMetroVersionNode(namespaceName);
+			AddAppManifestNode(namespaceName);
+		}
+		#endregion
+
+		#region AddMetroVersionNode
+		private void AddMetroVersionNode(string namespaceName)
+		{
 			XName propertyGroupName = XName.Get("PropertyGroup", namespaceName);
 			XName vsVersionName = XName.Get("VisualStudioVersion", namespaceName);
 
@@ -85,6 +127,25 @@ namespace ProjectConverter.Platforms
 				" '$(VisualStudioVersion)' == '' or '$(VisualStudioVersion)' < '11.0' "));
 			metroVersionElement.Add(new XElement(vsVersionName, "11.0"));
 			currentProject.Root.Add(metroVersionElement);
+		}
+		#endregion
+
+		#region AddAppManifestNode
+		private void AddAppManifestNode(string namespaceName)
+		{
+			if(isCurrentProjectExecutable == false)
+				return;
+
+			XName itemGroupName = XName.Get("ItemGroup", namespaceName);
+			XName appxManifestName = XName.Get("AppxManifest", namespaceName);
+			XName subTypeName = XName.Get("SubType", namespaceName);
+
+			XElement newItemGroup = new XElement(itemGroupName);
+			XElement appManifestElement = new XElement(appxManifestName);
+			newItemGroup.Add(appManifestElement);
+			appManifestElement.Add(new XAttribute("Include", "Manifest.appxmanifest"));
+			appManifestElement.Add(new XElement(subTypeName, "Designer"));
+			currentProject.Root.Add(newItemGroup);
 		}
 		#endregion
 	}
