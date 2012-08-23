9d456d10199d4a33529ac896397c8db94bbbbff1
Glatzemann <glatzemann@users.noreply.github.com>
2012-08-23 14:46:53 +0000

    - ProjectConverter is now able to: -- conditionally convert project types (controlled by cli argument) -- convert all projects of one or many solutions (passed as cli argument) -- prepared to work with single project files (exception currently)
    To get the old behaviour you have to call:
    
    ProjectConverter /linux /psvita /windowsmetro ../../ANX.Framework.sln

--- Tools/ProjectConverter/Program.cs
+++ Tools/ProjectConverter/Program.cs
@@ -1,7 +1,11 @@
+#region Using Statements
 using System;
 using System.Windows.Forms;
 using System.IO;
 using ProjectConverter.Platforms;
+using System.Collections.Generic;
+
+#endregion
 
 // This file is part of the ANX.Framework created by the
 // "ANX.Framework developer group" and released under the Ms-PL license.
@@ -11,17 +15,62 @@ namespace ProjectConverter
 {
 	static class Program
 	{
+        static Converter[] converters = new Converter[] { new LinuxConverter(), new MetroConverter(), new PsVitaConverter() };
+
 		[STAThread]
 		static void Main(string[] args)
 		{
-			Converter converter = new LinuxConverter();
-			converter.ConvertAllProjects("../../ANX.Framework.sln");
+            //
+            // To restore "old" behaviour use:
+            //   ProjectConverter /linux /psvita /windowsmetro ../../ANX.Framework.sln
+            //
 
-			converter = new PsVitaConverter();
-			converter.ConvertAllProjects("../../ANX.Framework.sln");
+            List<string> switches = new List<string>();
+            Dictionary<string, string> keyValueParameters = new Dictionary<string,string>();
+            List<string> files = new List<string>();
 
-			converter = new MetroConverter();
-			converter.ConvertAllProjects("../../ANX.Framework.sln");
+            foreach (string arg in args)
+            {
+                if (arg.StartsWith("/") || arg.StartsWith("-"))
+                {
+                    if (arg.Contains("="))
+                    {
+                        string[] parts = arg.Split('=');
+                        keyValueParameters[parts[0].Trim().ToLowerInvariant()] = parts[1].Trim().ToLowerInvariant();
+                    }
+                    else
+                    {
+                        switches.Add(arg.Substring(1).Trim().ToLowerInvariant());
+                    }
+                }
+                else
+                {
+                    if (File.Exists(arg))
+                    {
+                        files.Add(arg);
+                    }
+                }
+            }
+
+            foreach (string file in files)
+            {
+                string fileExt = Path.GetExtension(file).ToLowerInvariant();
+
+                foreach (Converter converter in converters)
+                {
+                    if (switches.Contains(converter.Postfix.ToLowerInvariant()))
+                    {
+                        switch (fileExt)
+                        {
+                            case ".sln":
+                                converter.ConvertAllProjects(file);
+                                break;
+                            default:
+                                throw new NotImplementedException("unsupported file type '" + fileExt + "'");
+                        }
+                    }
+                }
+            }
 		}
 	}
 }
